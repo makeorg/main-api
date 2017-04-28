38c43f253b30cdf208542f2901444dc6ffa22279
csalmon- <salmonl.colin@gmail.com>
2017-04-28 14:53:35 +0200

    Coordinator & ShardedActor for Proposition

---
+++ api/src/main/scala/org/make/api/proposition/PropositionCoordinator.scala
@@ -0,0 +1,28 @@
+package org.make.api.proposition
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
+import org.make.core.proposition.PropositionCommand
+
+object PropositionCoordinator {
+  def props: Props = Props(new PropositionActor)
+  def name: String = "proposition-coordinator"
+}
+
+class PropositionCoordinator extends Actor {
+  ClusterSharding(context.system).start(
+    ShardedProposition.shardName,
+    ShardedProposition.props,
+    ClusterShardingSettings(context.system),
+    ShardedProposition.extractEntityId,
+    ShardedProposition.extractShardId
+  )
+
+  def shardedProposition: ActorRef = {
+    ClusterSharding(context.system).shardRegion(ShardedProposition.shardName)
+  }
+
+  override def receive: Receive = {
+    case cmd: PropositionCommand =>
+  }
+}
---
+++ api/src/main/scala/org/make/api/proposition/ShardedProposition.scala
@@ -0,0 +1,43 @@
+package org.make.api.proposition
+
+import akka.actor.{Props, ReceiveTimeout}
+import akka.cluster.sharding.ShardRegion
+import akka.cluster.sharding.ShardRegion.Passivate
+import akka.persistence.{SaveSnapshotFailure, SaveSnapshotSuccess}
+import org.make.core.proposition.{PropositionCommand, PropositionId}
+
+import scala.concurrent.duration._
+
+object ShardedProposition {
+  def props: Props = Props(new ShardedProposition)
+
+  def name(propositionId: PropositionId): String = propositionId.value
+
+  val shardName: String = "proposition"
+
+  case object StopProposition
+
+  def extractEntityId: ShardRegion.ExtractEntityId = {
+    case cmd: PropositionCommand => (cmd.propositionId.value, cmd)
+  }
+
+  def extractShardId: ShardRegion.ExtractShardId = {
+    case cmd: PropositionCommand => Math.abs(cmd.propositionId.value.hashCode % 12).toString
+  }
+}
+
+class ShardedProposition extends PropositionActor {
+
+  import ShardedProposition._
+
+  context.setReceiveTimeout(2.minutes)
+
+  override def unhandled(msg: Any): Unit = msg match {
+    case ReceiveTimeout => context.parent ! Passivate(stopMessage = StopProposition)
+    case StopProposition => context.stop(self)
+    case SaveSnapshotSuccess(_) => println("Snapshot saved")
+    case SaveSnapshotFailure(_, cause) =>
+      println("Error while saving snapshot")
+      cause.printStackTrace()
+  }
+}
