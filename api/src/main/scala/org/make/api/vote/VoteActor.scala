77fb0c60d3b30041799128c259bb20e08ad63f82
cpestoury <cp@make.org>
2017-05-12 14:31:14 +0200

    update VoteActor

--- api/src/main/scala/org/make/api/vote/VoteActor.scala
+++ api/src/main/scala/org/make/api/vote/VoteActor.scala
@@ -34,36 +34,36 @@ class VoteActor extends PersistentActor with StrictLogging {
     case GetVote(voteId) => sender ! state.map(_.filter(_.voteId == voteId))
     case e: ViewVoteCommand =>
       persistAndPublishEvent(VotedView(id = e.voteId, propositionId = e.propositionId))
-    case agree: PutVoteCommand =>
+    case agree: PutVoteCommand if agree.status == VoteStatus.AGREE =>
       if (citizenCanVote(agree.citizenId))
         persistAndPublishEvent(VotedAgree(
           id = agree.voteId,
           propositionId = agree.propositionId,
           citizenId = agree.citizenId,
           createdAt = agree.createdAt,
-          status = VoteStatus.AGREE
+          status = agree.status
         ))
       Patterns.pipe((self ? GetVote(agree.voteId)) (1.second), Implicits.global).to(sender)
       self ! Snapshot
-    case disagree: PutVoteCommand =>
+    case disagree: PutVoteCommand if disagree.status == VoteStatus.DISAGREE =>
       if (citizenCanVote(disagree.citizenId))
         persistAndPublishEvent(VotedDisagree(
           id = disagree.voteId,
           propositionId = disagree.propositionId,
           citizenId = disagree.citizenId,
           createdAt = disagree.createdAt,
-          status = VoteStatus.DISAGREE
+          status = disagree.status
         ))
       Patterns.pipe((self ? GetVote(disagree.voteId)) (1.second), Implicits.global).to(sender)
       self ! Snapshot
-    case unsure: PutVoteCommand =>
+    case unsure: PutVoteCommand if unsure.status == VoteStatus.UNSURE =>
       if (citizenCanVote(unsure.citizenId))
         persistAndPublishEvent(VotedUnsure(
           id = unsure.voteId,
           propositionId = unsure.propositionId,
           citizenId = unsure.citizenId,
           createdAt = unsure.createdAt,
-          status = VoteStatus.DISAGREE
+          status = unsure.status
         ))
       Patterns.pipe((self ? GetVote(unsure.voteId)) (1.second), Implicits.global).to(sender)
       self ! Snapshot
