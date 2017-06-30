47a0c3e8c17063a0fb66c620b1d5530822208499
Francois LAROCHE <fl@make.org>
2017-06-30 14:51:24 +0200

    feat(transverse): do not throw exceptions on decode failure
    Use emap instead of map to allow the native circe error handling

--- core/src/main/scala/org/make/core/CirceFormatters.scala
+++ core/src/main/scala/org/make/core/CirceFormatters.scala
@@ -5,20 +5,37 @@ import java.util.UUID
 
 import io.circe._
 
+import scala.util.{Failure, Success, Try}
+
 trait CirceFormatters {
 
   implicit lazy val zonedDateTimeEncoder: Encoder[ZonedDateTime] =
     (a: ZonedDateTime) => Json.fromString(a.toString)
   implicit lazy val zonedDateTimeDecoder: Decoder[ZonedDateTime] =
-    Decoder.decodeString.map(ZonedDateTime.parse)
+    Decoder.decodeString.emap { date =>
+      Try(ZonedDateTime.parse(date)) match {
+        case Success(parsed) => Right(parsed)
+        case Failure(_)      => Left(s"$date is not a valid date, it should match yyyy-MM-ddThh:mm:ssZ")
+      }
+    }
 
   implicit lazy val localDateEncoder: Encoder[LocalDate] = (a: LocalDate) => Json.fromString(a.toString)
   implicit lazy val localDateDecoder: Decoder[LocalDate] =
-    Decoder.decodeString.map(LocalDate.parse)
+    Decoder.decodeString.emap { date =>
+      Try(LocalDate.parse(date)) match {
+        case Success(parsed) => Right(parsed)
+        case Failure(_)      => Left(s"$date is not a valid date, it should match yyyy-MM-dd")
+      }
+    }
 
   implicit lazy val uuidEncoder: Encoder[UUID] = (a: UUID) => Json.fromString(a.toString)
   implicit lazy val uuidDecoder: Decoder[UUID] =
-    Decoder.decodeString.map(UUID.fromString)
+    Decoder.decodeString.emap { uuid =>
+      Try(UUID.fromString(uuid)) match {
+        case Success(parsed) => Right(parsed)
+        case Failure(_)      => Left(s"$uuid is not a valid uuid")
+      }
+    }
 
 }
 
