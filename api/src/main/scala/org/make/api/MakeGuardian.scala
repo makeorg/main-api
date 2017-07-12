54b1720ad791c44e1606dcad5f9fb32c46865b89
arj-196 <arjruler93@gmail.com>
2017-07-12 15:21:26 +0200

    chore(user): initialize mailjet flow in MakeGuardian

--- api/src/main/scala/org/make/api/MakeGuardian.scala
+++ api/src/main/scala/org/make/api/MakeGuardian.scala
@@ -1,14 +1,15 @@
 package org.make.api
 
 import akka.actor.{Actor, ActorLogging, Props}
+import org.make.api.extensions.MailJetConfigurationExtension
 import org.make.api.proposition.PropositionSupervisor
 import org.make.api.technical.DeadLettersListenerActor
 import org.make.api.technical.cluster.ClusterFormationActor
-import org.make.api.technical.mailjet.MailJetProducerActor
+import org.make.api.technical.mailjet.{MailJet, MailJetProducerActor}
 import org.make.api.user.{UserService, UserSupervisor}
 import org.make.api.vote.VoteSupervisor
 
-class MakeGuardian(userService: UserService) extends Actor with ActorLogging {
+class MakeGuardian extends Actor with ActorLogging with MailJetConfigurationExtension {
 
   override def preStart(): Unit = {
     context.watch(context.actorOf(PropositionSupervisor.props, PropositionSupervisor.name))
@@ -20,6 +21,8 @@ class MakeGuardian(userService: UserService) extends Actor with ActorLogging {
     context.watch(context.actorOf(ClusterFormationActor.props, ClusterFormationActor.name))
     context.watch(context.actorOf(MailJetProducerActor.props, MailJetProducerActor.name))
     context.watch(context.actorOf(UserSupervisor.props, UserSupervisor.name))
+
+    val flow = MailJet.createFlow(mailJetConfiguration.apiKey, mailJetConfiguration.secretKey)
   }
 
   override def receive: Receive = {
@@ -29,5 +32,6 @@ class MakeGuardian(userService: UserService) extends Actor with ActorLogging {
 
 object MakeGuardian {
   val name: String = "make-api"
+
   def props(userService: UserService): Props = Props(new MakeGuardian(userService))
 }
