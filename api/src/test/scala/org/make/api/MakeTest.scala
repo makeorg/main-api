a35cff70f3589e5c5a4fafebe74698803733c738
csalmon- <salmonl.colin@gmail.com>
2017-07-17 11:04:22 +0200

    fix(transverse): add MakeTest trait in unit tests package

---
+++ api/src/test/scala/org/make/api/MakeTest.scala
@@ -0,0 +1,16 @@
+package org.make.api
+
+import com.typesafe.scalalogging.StrictLogging
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+
+// toDo: this trait must be shared between test and it
+trait MakeTest
+    extends FeatureSpec
+    with GivenWhenThen
+    with MockitoSugar
+    with Matchers
+    with StrictLogging
+    with BeforeAndAfterAll
+    with ScalaFutures {}
