393313cbe913467d9f629c59ea241580382baed4
csalmon- <salmonl.colin@gmail.com>
2017-08-09 17:04:45 +0200

    test(transverse): provide headers on rejections and exceptions

--- api/src/test/scala/org/make/api/MakeApiTestUtils.scala
+++ api/src/test/scala/org/make/api/MakeApiTestUtils.scala
@@ -1,14 +1,13 @@
 package org.make.api
 
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.directives.ExecutionDirectives
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.make.api.technical.{IdGeneratorComponent, MakeDirectives}
 
 trait MakeApiTestUtils extends MakeUnitTest with ScalatestRouteTest with MakeDirectives { this: IdGeneratorComponent =>
 
   def sealRoute(route: Route): Route =
-    ExecutionDirectives.handleExceptions(MakeApi.exceptionHandler)(
-      handleRejections(MakeApi.rejectionHandler)(Route.seal(route))
-    )
+    Route.seal(MakeApi.makeDefaultHeadersAndHandlers() {
+      route
+    })
 }
