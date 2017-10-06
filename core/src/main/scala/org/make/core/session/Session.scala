1d4d0b99bce17823d9a0819f4830d63ca0471178
cpestoury <cp@make.org>
2017-10-06 15:44:47 +0200

    fix(session): add encoder and decoder for sessionid

--- core/src/main/scala/org/make/core/session/Session.scala
+++ core/src/main/scala/org/make/core/session/Session.scala
@@ -1,11 +1,17 @@
 package org.make.core.session
 
+import io.circe.{Decoder, Encoder, Json}
 import org.make.core.StringValue
 import spray.json.{JsString, JsValue, JsonFormat}
 
 final case class SessionId(value: String) extends StringValue
 
 object SessionId {
+
+  implicit lazy val sessionIdEncoder: Encoder[SessionId] =
+    (a: SessionId) => Json.fromString(a.value)
+  implicit lazy val sessionIdDecoder: Decoder[SessionId] =
+    Decoder.decodeString.map(SessionId(_))
 
   implicit val sessionIdFormatter: JsonFormat[SessionId] = new JsonFormat[SessionId] {
     override def read(json: JsValue): SessionId = json match {
