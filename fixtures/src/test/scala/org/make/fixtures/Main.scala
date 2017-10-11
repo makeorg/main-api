6486d0282123854378f5b6164f6301b708e897b0
Amine Zaghdoudi <amine.zaghdoudi@gmail.com>
2017-10-11 11:50:47 +0200

    fix(proposal): theme with right tag + fix import proposal with right user

--- fixtures/src/test/scala/org/make/fixtures/Main.scala
+++ fixtures/src/test/scala/org/make/fixtures/Main.scala
@@ -1,15 +1,64 @@
 package org.make.fixtures
 
-import io.gatling.core.Predef.{heavisideUsers, nothingFor, Simulation}
+import java.util.concurrent.ThreadLocalRandom
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.Record
+import io.gatling.core.json.Json
+import io.gatling.http.Predef.http
+import io.gatling.http.protocol.HttpProtocolBuilder
+import org.make.fixtures.Proposal.proposalsByUsername
+import org.make.fixtures.User._
 
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
 class Main extends Simulation {
+  val maxClients = 710
+  val httpConf: HttpProtocolBuilder = http
+    .baseURL(baseURL)
+    .acceptHeader("*/*")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader(defaultAcceptLanguage)
+    .userAgentHeader(defaultUserAgent)
+    .header("x-make-operation", "")
+    .header("x-make-source", "core")
+    .header("x-make-location", "homepage")
+    .header("x-make-question", "")
+    .disableCaching
+
   setUp(
-    User.scnRegister
-      .inject(heavisideUsers(User.maxClients).over(30.minutes))
-      .protocols(User.httpConf),
-    Proposal.scnRegister
-      .inject(nothingFor(30.seconds), heavisideUsers(Proposal.maxClients).over(30.minutes))
-      .protocols(Proposal.httpConf)
+    scenario("Register user and create proposal")
+      .feed(User.userFeeder)
+      .exec(UserChainBuilder.createUser)
+      .exec(UserChainBuilder.authenticate(UserAuthParams(username = "${username}", password = "${password}")))
+      .exec(
+        session =>
+          session("username").validate[String].map { username: String =>
+            val mayBeProposals: Option[IndexedSeq[Record[String]]] = Try(Some(proposalsByUsername(username))) match {
+              case Success(indexedSeqOption) => indexedSeqOption
+              case Failure(_)                => None
+            }
+
+            mayBeProposals.map { proposals =>
+              proposals(ThreadLocalRandom.current.nextInt(proposals.length))
+              val selectedProposal = proposals(ThreadLocalRandom.current.nextInt(proposals.length))
+              val tags = Json.stringify(selectedProposal("tags").split('|').toSeq)
+              session
+                .set("content", selectedProposal("content"))
+                .set("theme", selectedProposal("theme"))
+                .set("tags", tags)
+            }.getOrElse(session)
+        }
+      )
+      .doIf(session => session.attributes.get("content").nonEmpty) {
+        exec(
+          ProposalChainBuilder.createProposal,
+          UserChainBuilder.authenticateAsAdmin,
+          ProposalChainBuilder.acceptProposal
+        )
+      }
+      .inject(heavisideUsers(maxClients).over(15.minutes))
+      .protocols(httpConf)
   )
 }
--- fixtures/src/test/scala/org/make/fixtures/Proposal.scala
+++ fixtures/src/test/scala/org/make/fixtures/Proposal.scala
@@ -1,70 +1,15 @@
 package org.make.fixtures
 
-import java.util.concurrent.ThreadLocalRandom
-
 import io.gatling.core.Predef._
 import io.gatling.core.feeder._
-import io.gatling.core.json.Json
-import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
-import io.gatling.http.Predef.{http, jsonPath, status}
-import io.gatling.http.protocol.HttpProtocolBuilder
-
-import scala.util.{Failure, Success, Try}
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef.{jsonPath, status}
 
 object Proposal extends SimulationConfig {
-
-  val maxClients = 710
-  val httpConf: HttpProtocolBuilder = http
-    .baseURL(baseURL)
-    .acceptHeader("*/*")
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader(defaultAcceptLanguage)
-    .userAgentHeader(defaultUserAgent)
-    .header("x-make-operation", "")
-    .header("x-make-source", "core")
-    .header("x-make-location", "homepage")
-    .header("x-make-question", "")
-    .disableCaching
 
   val proposalsByUsername: Map[String, IndexedSeq[Record[String]]] =
     ssv(proposalFeederPath, '"', '\\').records.groupBy { record =>
       record("username")
-    }
-
-  private val userFeeder = ssv(userFeederPath, '"', '\\')
-  private val defaultPause = 2
-
-  val scnRegister: ScenarioBuilder = scenario("Create proposal with theme")
-    .feed(userFeeder.circular)
-    .exec(
-      session =>
-        session("username").validate[String].map { username: String =>
-          val mayBeProposals: Option[IndexedSeq[Record[String]]] = Try(Some(proposalsByUsername(username))) match {
-            case Success(indexedSeqOption) => indexedSeqOption
-            case Failure(_)                => None
-          }
-
-          mayBeProposals.map { proposals =>
-            proposals(ThreadLocalRandom.current.nextInt(proposals.length))
-            val selectedProposal = proposals(ThreadLocalRandom.current.nextInt(proposals.length))
-            val tags = Json.stringify(selectedProposal("tags").split('|').toSeq)
-            session
-              .set("content", selectedProposal("content"))
-              .set("theme", selectedProposal("theme"))
-              .set("tags", tags)
-          }.getOrElse(session)
-      }
-    )
-    .doIf(session => session.attributes.get("content").nonEmpty) {
-      exec(
-        UserChainBuilder
-          .authenticate(UserAuthParams(username = "${username}", password = "${password}"))
-          .pause(defaultPause),
-        ProposalChainBuilder.createProposal
-          .pause(defaultPause),
-        UserChainBuilder.authenticateAsAdmin,
-        ProposalChainBuilder.acceptProposal
-      )
     }
 }
 
@@ -73,7 +18,6 @@ object ProposalChainBuilder {
   private val statusOk = 200
 
   val createProposal: ChainBuilder = {
-
     exec(
       MakeServicesBuilder.createProposalBuilder
         .header("x-make-theme-id", "${theme}")
@@ -85,7 +29,6 @@ object ProposalChainBuilder {
   }
 
   val acceptProposal: ChainBuilder = {
-
     exec(
       MakeServicesBuilder.acceptProposalBuilder
         .body(ElFileBody("jsonModel/validateProposal.json"))
