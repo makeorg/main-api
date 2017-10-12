ba4eba5c7aef57eae5ef3a2396934db37578c1d3
csalmon- <salmonl.colin@gmail.com>
2017-10-12 16:42:06 +0200

    feat(transverse): add tags for VFF into business config

--- api/src/main/scala/org/make/api/technical/businessconfig/BusinessConfig.scala
+++ api/src/main/scala/org/make/api/technical/businessconfig/BusinessConfig.scala
@@ -1,16 +1,18 @@
 package org.make.api.technical.businessconfig
 
-import org.make.core.reference.Theme
+import org.make.core.reference.{Tag, Theme}
 
 sealed trait BusinessConfig {
   val proposalMinLength: Int
   val proposalMaxLength: Int
   val themes: Seq[Theme]
+  val tagsVFF: Seq[Tag]
 }
 
 case class BackofficeConfiguration(override val proposalMinLength: Int,
                                    override val proposalMaxLength: Int,
                                    override val themes: Seq[Theme],
+                                   override val tagsVFF: Seq[Tag],
                                    nVotesTriggerConnexion: Int,
                                    nPendingProposalsTriggerEmailModerator: Int,
                                    minProposalsPerSequence: Int,
@@ -21,6 +23,7 @@ case class BackofficeConfiguration(override val proposalMinLength: Int,
 case class FrontConfiguration(override val proposalMinLength: Int,
                               override val proposalMaxLength: Int,
                               override val themes: Seq[Theme],
+                              override val tagsVFF: Seq[Tag],
                               newVisitorCookieDefinition: String)
     extends BusinessConfig
 
@@ -28,6 +31,20 @@ object BusinessConfig {
   val defaultProposalMinLength: Int = 12
   val defaultProposalMaxLength: Int = 140
   val themes: Seq[Theme] = Seq.empty
+  val tagsVFF: Seq[Tag] = Seq(
+    Tag("signalement"),
+    Tag("police & justice"),
+    Tag("image de la femme"),
+    Tag("cyber-harcèlement"),
+    Tag("protection des victimes"),
+    Tag("transports"),
+    Tag("action publique"),
+    Tag("hébergement"),
+    Tag("pédagogie"),
+    Tag("soutien psychologique"),
+    Tag("plaintes"),
+    Tag("agressions")
+  )
 }
 
 object FrontConfiguration {
@@ -36,11 +53,13 @@ object FrontConfiguration {
   def default(proposalMinLength: Int = BusinessConfig.defaultProposalMinLength,
               proposalMaxLength: Int = BusinessConfig.defaultProposalMaxLength,
               themes: Seq[Theme] = BusinessConfig.themes,
+              tagsVFF: Seq[Tag] = BusinessConfig.tagsVFF,
               newVisitorCookieDefinition: String = defaultNewVisitorCookieDefinition): FrontConfiguration =
     FrontConfiguration(
       proposalMinLength = proposalMinLength,
       proposalMaxLength = proposalMaxLength,
       themes = themes,
+      tagsVFF = tagsVFF,
       newVisitorCookieDefinition = newVisitorCookieDefinition
     )
 }
@@ -57,6 +76,7 @@ object BackofficeConfiguration {
   def default(proposalMinLength: Int = BusinessConfig.defaultProposalMinLength,
               proposalMaxLength: Int = BusinessConfig.defaultProposalMaxLength,
               themes: Seq[Theme] = BusinessConfig.themes,
+              tagsVFF: Seq[Tag] = BusinessConfig.tagsVFF,
               nVotesTriggerConnexion: Int = defaultNumberVotesTriggerConnexion,
               nPendingProposalsTriggerEmailModerator: Int = defaultNumberPendingProposalsTriggerEmailModerator,
               minProposalsPerSequence: Int = defaultMinProposalsPerSequence,
@@ -66,6 +86,7 @@ object BackofficeConfiguration {
       proposalMinLength = proposalMinLength,
       proposalMaxLength = proposalMaxLength,
       themes = themes,
+      tagsVFF = tagsVFF,
       reasonsForRefusal = reasonsForRefusal,
       nVotesTriggerConnexion = nVotesTriggerConnexion,
       nPendingProposalsTriggerEmailModerator = nPendingProposalsTriggerEmailModerator,
