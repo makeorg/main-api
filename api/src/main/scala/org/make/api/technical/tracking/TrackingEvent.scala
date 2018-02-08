6bf92e4007ce442af4494eea5692b5a0094ff7d9
David <dm@make.org>
2018-02-08 14:56:52 +0100

    feat(transverse): Wrap tracking event

--- api/src/main/scala/org/make/api/technical/tracking/TrackingEvent.scala
+++ api/src/main/scala/org/make/api/technical/tracking/TrackingEvent.scala
@@ -2,7 +2,8 @@ package org.make.api.technical.tracking
 
 import java.time.ZonedDateTime
 
-import org.make.core.{DateHelper, RequestContext}
+import org.make.core.{DateHelper, EventWrapper, MakeSerializable, RequestContext}
+import shapeless.{:+:, CNil, Coproduct}
 
 final case class TrackingEvent(eventProvider: String,
                                eventType: Option[String],
@@ -11,9 +12,18 @@ final case class TrackingEvent(eventProvider: String,
                                requestContext: RequestContext,
                                createdAt: ZonedDateTime)
 
+final case class TrackingEventWrapper(version: Int,
+                                      id: String,
+                                      date: ZonedDateTime,
+                                      eventType: String,
+                                      event: Coproduct)
+    extends EventWrapper
+
 object TrackingEvent {
-  def eventfromFront(frontRequest: FrontTrackingRequest, requestContext: RequestContext): TrackingEvent =
-    TrackingEvent(
+  type AnyTrackingEvent = TrackingEvent :+: CNil
+
+  def eventfromFront(frontRequest: FrontTrackingRequest, requestContext: RequestContext): TrackingEventWrapper = {
+    val trackingEvent = TrackingEvent(
       eventProvider = "front",
       eventType = Some(frontRequest.eventType),
       eventName = frontRequest.eventName,
@@ -21,4 +31,13 @@ object TrackingEvent {
       requestContext = requestContext,
       createdAt = DateHelper.now()
     )
+
+    TrackingEventWrapper(
+      version = MakeSerializable.V1,
+      id = requestContext.sessionId.value,
+      date = trackingEvent.createdAt,
+      eventType = "TrackingEvent",
+      event = Coproduct[AnyTrackingEvent](trackingEvent)
+    )
+  }
 }
