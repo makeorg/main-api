891f8f7fa5f46f2244ded68f2d307f6a451d3739
Francois LAROCHE <fl@make.org>
2018-03-20 17:41:51 +0100

    fix(transverse): ensure all events provide a sharding key
    shard everything in kafka in order to avoid conflicts with consumers
    
    Signed-off-by: Francois LAROCHE <fl@make.org>

--- core/src/main/scala/org/make/core/EventWrapper.scala
+++ core/src/main/scala/org/make/core/EventWrapper.scala
@@ -4,10 +4,13 @@ import java.time.ZonedDateTime
 
 import shapeless.Coproduct
 
-trait EventWrapper {
+trait EventWrapper extends Sharded {
   def version: Int
-  def id: String
   def date: ZonedDateTime
   def eventType: String
   def event: Coproduct
+}
+
+trait Sharded {
+  def id: String
 }
