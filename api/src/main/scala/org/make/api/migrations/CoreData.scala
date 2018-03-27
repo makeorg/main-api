b3e35c7765af6dfb2d4ca6c1d42fa19efe6aa24a
Colin Salmon-Legagneur <salmonl.colin@gmail.com>
2018-03-27 16:17:08 +0200

    feat(operation): create make-europe
    Closes #225

--- api/src/main/scala/org/make/api/migrations/CoreData.scala
+++ api/src/main/scala/org/make/api/migrations/CoreData.scala
@@ -1,11 +1,14 @@
 package org.make.api.migrations
 
 import org.make.api.migrations.InsertFixtureData.FixtureDataLine
+import org.make.core.RequestContext
 import org.make.core.reference.{LabelId, TagId, ThemeId}
 
 object CoreData extends InsertFixtureData {
 
   override val dataResource: String = "fixtures/proposals.csv"
+  var localRequestContext: RequestContext = RequestContext.empty
+  override def requestContext: RequestContext = localRequestContext
 
   override def extractDataLine(line: String): Option[FixtureDataLine] = {
     line.drop(1).dropRight(1).split("""";"""") match {
---
+++ api/src/main/scala/org/make/api/migrations/MakeEuropeOperation.scala
@@ -0,0 +1,34 @@
+package org.make.api.migrations
+
+import java.time.LocalDate
+
+import org.make.core.reference.TagId
+
+object MakeEuropeOperation extends CreateOperation {
+  override val operationSlug: String = "make-europe"
+
+  override val defaultLanguage: String = "en"
+
+  override val countryConfigurations: Seq[CreateOperation.CountryConfiguration] = Seq(
+    CreateOperation.CountryConfiguration(
+      country = "GB",
+      language = "en",
+      title = "Consultation européenne (démo)",
+      startDate = LocalDate.parse("2018-03-26"),
+      endDate = Some(LocalDate.parse("2018-04-30")),
+      tags = Seq(
+        TagId("european-commission"),
+        TagId("social"),
+        TagId("taxes"),
+        TagId("environment"),
+        TagId("minimum-wage"),
+        TagId("finance"),
+        TagId("budget"),
+        TagId("students"),
+        TagId("erasmus")
+      )
+    )
+  )
+
+  override val runInProduction: Boolean = true
+}
