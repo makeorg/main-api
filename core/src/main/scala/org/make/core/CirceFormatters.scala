2b538e8f4e06eac3cd206deca9892b0a40a6c682
Philippe de Araujo <pa@make.org>
2018-05-11 06:56:59 +0200

    feat(transverse): allow bad types from mailjet
    closes #269

--- core/src/main/scala/org/make/core/CirceFormatters.scala
+++ core/src/main/scala/org/make/core/CirceFormatters.scala
@@ -43,6 +43,11 @@ trait CirceFormatters {
       }
     }
 
+  implicit def h[A, B](implicit a: Decoder[A], b: Decoder[B]): Decoder[Either[A, B]] = {
+    val l: Decoder[Either[A, B]] = a.map(Left.apply)
+    val r: Decoder[Either[A, B]] = b.map(Right.apply)
+    l.or(r)
+  }
 }
 
 // object CirceFormatters extends CirceFormatters
