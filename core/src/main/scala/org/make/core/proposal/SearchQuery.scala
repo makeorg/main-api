9854742bd591cf2923e1083700f4711151211c86
Colin Salmon-Legagneur <salmonl.colin@gmail.com>
2018-06-25 17:10:11 +0200

    feat(proposal): setup sort algorithm for ES queries
    * add random sort algorithm
    
    Closes #298

--- core/src/main/scala/org/make/core/proposal/SearchQuery.scala
+++ core/src/main/scala/org/make/core/proposal/SearchQuery.scala
@@ -21,6 +21,8 @@ package org.make.core.proposal
 
 import com.sksamuel.elastic4s.ElasticApi
 import com.sksamuel.elastic4s.http.ElasticDsl
+import com.sksamuel.elastic4s.http.ElasticDsl.{functionScoreQuery, randomScore}
+import com.sksamuel.elastic4s.searches.SearchDefinition
 import com.sksamuel.elastic4s.searches.queries.QueryDefinition
 import com.sksamuel.elastic4s.searches.sort.FieldSortDefinition
 import org.elasticsearch.search.sort.SortOrder
@@ -424,3 +426,24 @@ case class IdeaSearchFilter(ideaId: IdeaId)
 case class Limit(value: Int)
 
 case class Skip(value: Int)
+
+// Sort Algorithms
+
+sealed trait SortAlgorithm {
+  val shortName: String
+  def sortDefinition(request: SearchDefinition): SearchDefinition
+}
+
+case class RandomAlgorithm(maybeSeed: Option[Int] = None) extends SortAlgorithm {
+  override val shortName: String = "random"
+
+  override def sortDefinition(request: SearchDefinition): SearchDefinition = {
+    (
+      for {
+        seed  <- maybeSeed
+        query <- request.query
+      } yield request.query(functionScoreQuery().query(query).scorers(randomScore(seed)))
+    ).getOrElse(request)
+
+  }
+}
