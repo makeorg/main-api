2469625d75633a1d1a0e27b08f83c9cda1f9981b
François LAROCHE <fl@make.org>
2018-08-14 12:03:08 +0200

    fix(user): use users from db in fixtures in priority
    also consume messages sequencially to avoid flooding ES
    in the sequence consumer
    
    Signed-off-by: François LAROCHE <fl@make.org>

--- api/src/main/scala/org/make/api/migrations/HuffingPostOperations.scala
+++ api/src/main/scala/org/make/api/migrations/HuffingPostOperations.scala
@@ -45,7 +45,7 @@ object HuffingPostOperations extends Migration {
   implicit val executor: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
   val emptyContext: RequestContext = RequestContext.empty
   val moderatorId = UserId("11111111-1111-1111-1111-111111111111")
-  var huffingPostUser: Seq[User] = _
+  var huffingPostUser: Seq[User] = Seq.empty
 
   val operations: Map[String, CreateOperation.CountryConfiguration] = Map(
     "politique-huffpost" -> CountryConfiguration(
@@ -132,55 +132,59 @@ object HuffingPostOperations extends Migration {
 
   override def migrate(api: MakeApi): Future[Unit] = {
 
-      sequentially(operations.toSeq) {
-        case (operationSlug, countryConfiguration) =>
-          // create Sequence and Operation
-          retryableFuture(
-            api.sequenceService
-              .create(
-                userId = moderatorId,
-                requestContext = emptyContext,
-                createdAt = DateHelper.now(),
-                title = operationSlug,
-                themeIds = Seq.empty,
-                operationId = None,
-                searchable = true
+    sequentially(operations.toSeq) {
+      case (operationSlug, countryConfiguration) =>
+        // create Sequence and Operation
+        retryableFuture(
+          api.sequenceService
+            .create(
+              userId = moderatorId,
+              requestContext = emptyContext,
+              createdAt = DateHelper.now(),
+              title = operationSlug,
+              themeIds = Seq.empty,
+              operationId = None,
+              searchable = true
+            )
+            .map(_.get)
+            .map { sequenceResponse =>
+              SequenceWithCountryLanguage(
+                sequence = sequenceResponse,
+                country = countryConfiguration.country,
+                language = countryConfiguration.language
               )
-              .map(_.get)
-              .map { sequenceResponse =>
-                SequenceWithCountryLanguage(
-                  sequence = sequenceResponse,
-                  country = countryConfiguration.country,
-                  language = countryConfiguration.language
-                )
-              }
-          ).map { sequenceWithCountryLanguage =>
-              api.operationService.findOneBySlug(operationSlug).flatMap {
-                case Some(operation) => Future.successful(operation.operationId)
-                case None =>
-                  api.operationService
-                    .create(
-                      userId = moderatorId,
-                      slug = operationSlug,
-                      defaultLanguage = defaultLanguage,
-                      translations = Seq(
-                        OperationTranslation(
-                          title = countryConfiguration.title,
-                          language = sequenceWithCountryLanguage.language
-                        )
-                      ),
-                      countriesConfiguration = Seq(
-                        OperationCountryConfiguration(
-                          countryCode = sequenceWithCountryLanguage.country,
-                          tagIds = countryConfiguration.tags,
-                          landingSequenceId = sequenceWithCountryLanguage.sequence.sequenceId,
-                          startDate = Some(countryConfiguration.startDate),
-                          endDate = countryConfiguration.endDate
-                        )
+            }
+        ).map { sequenceWithCountryLanguage =>
+          api.operationService
+            .findOneBySlug(operationSlug)
+            .flatMap {
+              case Some(operation) => Future.successful(operation.operationId)
+              case None =>
+                api.operationService
+                  .create(
+                    userId = moderatorId,
+                    slug = operationSlug,
+                    defaultLanguage = defaultLanguage,
+                    translations = Seq(
+                      OperationTranslation(
+                        title = countryConfiguration.title,
+                        language = sequenceWithCountryLanguage.language
+                      )
+                    ),
+                    countriesConfiguration = Seq(
+                      OperationCountryConfiguration(
+                        countryCode = sequenceWithCountryLanguage.country,
+                        tagIds = countryConfiguration.tags,
+                        landingSequenceId = sequenceWithCountryLanguage.sequence.sequenceId,
+                        startDate = Some(countryConfiguration.startDate),
+                        endDate = countryConfiguration.endDate
                       )
                     )
-              }.map { operationId =>
-                retryableFuture(api.sequenceService.update(
+                  )
+            }
+            .map { operationId =>
+              retryableFuture(
+                api.sequenceService.update(
                   sequenceId = sequenceWithCountryLanguage.sequence.sequenceId,
                   moderatorId = moderatorId,
                   requestContext = emptyContext,
@@ -188,36 +192,51 @@ object HuffingPostOperations extends Migration {
                   status = Some(SequenceStatus.Published),
                   operationId = Some(operationId),
                   themeIds = Seq.empty
-                ))
+                )
+              )
 
-                val tags: Seq[ImportTagsData.TagsDataLine] =
-                  Source.fromResource(operationsTagsSource(operationSlug)).getLines().toSeq.drop(1).flatMap(ImportTagsData.extractDataLine)
+              val tags: Seq[ImportTagsData.TagsDataLine] =
+                Source
+                  .fromResource(operationsTagsSource(operationSlug))
+                  .getLines()
+                  .toSeq
+                  .drop(1)
+                  .flatMap(ImportTagsData.extractDataLine)
 
-                // create Tags of the operation
-                api.tagService.findByOperationId(operationId).flatMap { foundTags =>
+              // create Tags of the operation
+              api.tagService
+                .findByOperationId(operationId)
+                .flatMap { foundTags =>
                   if (foundTags.nonEmpty) {
                     Future.successful(foundTags)
                   } else {
-                    Future.traverse(tags) { tag =>
-                      api.tagService
-                        .createTag(
-                          label = tag.label,
-                          tagTypeId = tag.tagTypeId,
-                          operationId = Some(operationId),
-                          themeId = None,
-                          country = tag.country,
-                          language = tag.language,
-                          display = tag.tagDisplay,
-                          weight = tag.weight
-                        )
-                    }.map { createdTags =>
-                      createdTags
-                    }
+                    Future
+                      .traverse(tags) { tag =>
+                        api.tagService
+                          .createTag(
+                            label = tag.label,
+                            tagTypeId = tag.tagTypeId,
+                            operationId = Some(operationId),
+                            themeId = None,
+                            country = tag.country,
+                            language = tag.language,
+                            display = tag.tagDisplay,
+                            weight = tag.weight
+                          )
+                      }
+                      .map { createdTags =>
+                        createdTags
+                      }
                   }
-                }.map { operationTags =>
-
+                }
+                .map { operationTags =>
                   val proposalsToInsert: Seq[ProposalsDataLine] =
-                    Source.fromResource(operationsProposalsSource(operationSlug)).getLines().toSeq.drop(1).flatMap(extractProposalDataLine)
+                    Source
+                      .fromResource(operationsProposalsSource(operationSlug))
+                      .getLines()
+                      .toSeq
+                      .drop(1)
+                      .flatMap(extractProposalDataLine)
 
                   // Traverse the proposals insert and validate theme
                   sequentially(proposalsToInsert) { proposalToInsert =>
@@ -232,16 +251,16 @@ object HuffingPostOperations extends Migration {
                             )
                           )
                         )
-                      ).flatMap { countResult =>
+                      )
+                      .flatMap { countResult =>
                         if (countResult > 0) {
                           Future.successful {}
                         } else {
-                          val tagsOfProposal: Seq[Tag] = operationTags.filter(tag =>  proposalToInsert.tagsLabel.contains(tag.label))
+                          val tagsOfProposal: Seq[Tag] =
+                            operationTags.filter(tag => proposalToInsert.tagsLabel.contains(tag.label))
 
                           for {
-                            user <- retryableFuture(
-                              retrieveOrInsertUser(api = api, email = proposalToInsert.email)
-                            )
+                            user <- retryableFuture(retrieveOrInsertUser(api = api, email = proposalToInsert.email))
                             idea <- retryableFuture(
                               retrieveOrInsertIdea(
                                 api = api,
@@ -252,56 +271,54 @@ object HuffingPostOperations extends Migration {
                                 themeId = None
                               )
                             )
-                            proposalId <-
-                              retryableFuture(
-                                api.proposalService
-                                  .propose(
-                                    user,
-                                    RequestContext.empty.copy(
-                                      question = Some(operationSlug),
-                                      source = Some("huffingpost"),
-                                      operationId = Some(operationId)
-                                    ),
-                                    DateHelper.now(),
-                                    proposalToInsert.content,
-                                    Some(operationId),
-                                    None,
-                                    Some(proposalToInsert.language),
-                                    Some(proposalToInsert.country)
-                                  )
-                              )
-                            _ <-
-                              retryableFuture(
-                                api.proposalService.validateProposal(
-                                  proposalId,
-                                  moderatorId,
+                            proposalId <- retryableFuture(
+                              api.proposalService
+                                .propose(
+                                  user,
                                   RequestContext.empty.copy(
                                     question = Some(operationSlug),
                                     source = Some("huffingpost"),
                                     operationId = Some(operationId)
                                   ),
-                                  ValidateProposalRequest(
-                                    newContent = None,
-                                    sendNotificationEmail = false,
-                                    theme = None,
-                                    labels = Seq.empty,
-                                    tags = tagsOfProposal.map(_.tagId),
-                                    similarProposals = Seq.empty,
-                                    operation = Some(operationId),
-                                    idea = Some(idea.ideaId)
-                                  )
+                                  DateHelper.now(),
+                                  proposalToInsert.content,
+                                  Some(operationId),
+                                  None,
+                                  Some(proposalToInsert.language),
+                                  Some(proposalToInsert.country)
+                                )
+                            )
+                            _ <- retryableFuture(
+                              api.proposalService.validateProposal(
+                                proposalId,
+                                moderatorId,
+                                RequestContext.empty.copy(
+                                  question = Some(operationSlug),
+                                  source = Some("huffingpost"),
+                                  operationId = Some(operationId)
+                                ),
+                                ValidateProposalRequest(
+                                  newContent = None,
+                                  sendNotificationEmail = false,
+                                  theme = None,
+                                  labels = Seq.empty,
+                                  tags = tagsOfProposal.map(_.tagId),
+                                  similarProposals = Seq.empty,
+                                  operation = Some(operationId),
+                                  idea = Some(idea.ideaId)
                                 )
                               )
+                            )
                           } yield {}
                         }
                       }
                   }
                 }
-              }
-          }
+            }
+        }
 
-        case _ => Future.failed(new RuntimeException("Error When importingHuffingPost operations"))
-      }
+      case _ => Future.failed(new RuntimeException("Error When importingHuffingPost operations"))
+    }
   }
 
   def retrieveOrInsertIdea(api: MakeApi,
@@ -324,10 +341,8 @@ object HuffingPostOperations extends Migration {
     }
   }
 
-  def retrieveOrInsertUser(api: MakeApi,
-                           email: String): Future[User] = {
+  def retrieveOrInsertUser(api: MakeApi, email: String): Future[User] = {
     val annonymousEmail: String = "yopmail+annonymous@make.org"
-
 
     // If annonymous is the user proposal pick a random yopmail user
     if (annonymousEmail.equals(email)) {
@@ -335,11 +350,10 @@ object HuffingPostOperations extends Migration {
     } else {
       api.persistentUserService.findByEmail(email).flatMap {
         case Some(user) => Future.successful(user)
-        case None => Future.successful(huffingPostUser(Random.nextInt(huffingPostUser.size)))
+        case None       => Future.successful(huffingPostUser(Random.nextInt(huffingPostUser.size)))
       }
     }
   }
-
 
   def createUsers(api: MakeApi) = {
     def agedUser(email: String, firstName: String, age: Int): User =
@@ -364,7 +378,7 @@ object HuffingPostOperations extends Migration {
         createdAt = Some(DateHelper.now())
       )
 
-    huffingPostUser = Seq(
+    val usersToInsert = Seq(
       agedUser("yopmail+leila@make.org", "Leila", 33),
       agedUser("yopmail+ariane@make.org", "Ariane", 19),
       agedUser("yopmail+aminata@make.org", "Aminata", 45),
@@ -395,12 +409,14 @@ object HuffingPostOperations extends Migration {
       agedUser("yopmail+sylvain@make.org", "Sylvain", 46)
     )
 
-    sequentially(huffingPostUser) { user =>
+    sequentially(usersToInsert) { user =>
       api.persistentUserService
         .findByEmail(user.email)
         .flatMap {
-          case Some(_) => Future.successful {}
-          case None    => api.persistentUserService.persist(user).map(_ => ())
+          case Some(u) =>
+            huffingPostUser ++= Seq(u)
+            Future.successful {}
+          case None => api.persistentUserService.persist(user).map(u => huffingPostUser ++= Seq(u))
         }
 
     }.recoverWith {
