4cdf522e49388f0beddf18f25a69653bbb16e139
François LAROCHE <fl@make.org>
2018-12-19 14:48:54 +0100

    feat(transverse): Add indexes on most used tables
    Signed-off-by: François LAROCHE <fl@make.org>

---
+++ api/src/main/resources/db/migration/V26__User_and_token_indexes.sql
@@ -0,0 +1,19 @@
+CREATE INDEX ON make_user(email);
+CREATE INDEX ON make_user(verification_token);
+CREATE INDEX ON make_user(reset_token);
+CREATE INDEX ON make_user(public_profile);
+CREATE INDEX ON make_user(is_organisation);
+
+CREATE INDEX ON access_token(refresh_token);
+
+CREATE INDEX ON oauth_client(secret);
+
+CREATE INDEX ON operation(slug);
+
+CREATE INDEX ON question(slug);
+
+CREATE INDEX ON sequence_configuration(question_id);
+
+CREATE INDEX ON tag(question_id);
+
+COMMIT;
