d2f0efd0164c88b7ba78b0e6be9937803c1b6ff2
François LAROCHE <fl@make.org>
2020-01-20 14:18:32 +0100

    feat(user): new personality profile
    Signed-off-by: Francois LAROCHE <fl@make.org>

--- api/src/main/scala/org/make/api/personality/PersonalityApi.scala
+++ api/src/main/scala/org/make/api/personality/PersonalityApi.scala
@@ -9,6 +9,15 @@ import org.make.api.technical.{IdGeneratorComponent, MakeAuthenticationDirective
 import org.make.api.user.{UserResponse, UserServiceComponent}
 import org.make.core._
 import org.make.core.user.UserId
+import io.circe.generic.semiauto._
+import io.circe.Encoder
+import io.circe.Decoder
+import org.make.core.user.User
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string.Url
+import scala.annotation.meta.field
+import io.circe.refined._
+import org.make.core.profile.Profile
 
 @Api(value = "Personalities")
 @Path(value = "/personalities")
@@ -29,7 +38,50 @@ trait PersonalityApi extends Directives {
   @Path(value = "/{userId}")
   def getPersonality: Route
 
-  def routes: Route = getPersonality
+  @ApiOperation(value = "get-personality", httpMethod = "GET", code = HttpCodes.OK)
+  @ApiResponses(
+    value = Array(new ApiResponse(code = HttpCodes.OK, message = "Ok", response = classOf[PersonalityProfileResponse]))
+  )
+  @ApiImplicitParams(
+    value = Array(
+      new ApiImplicitParam(
+        name = "userId",
+        paramType = "path",
+        dataType = "string",
+        example = "d22c8e70-f709-42ff-8a52-9398d159c753"
+      )
+    )
+  )
+  @Path(value = "/{userId}/profile")
+  def getPersonalityProfile: Route
+
+  @ApiOperation(
+    value = "update-personality-profile",
+    httpMethod = "PUT",
+    authorizations = Array(
+      new Authorization(
+        value = "MakeApi",
+        scopes = Array(new AuthorizationScope(scope = "user", description = "application user"))
+      )
+    )
+  )
+  @ApiResponses(
+    value = Array(new ApiResponse(code = HttpCodes.OK, message = "Ok", response = classOf[PersonalityProfileResponse]))
+  )
+  @ApiImplicitParams(
+    value = Array(
+      new ApiImplicitParam(name = "userId", paramType = "path", dataType = "string"),
+      new ApiImplicitParam(
+        value = "body",
+        paramType = "body",
+        dataType = "org.make.api.personality.PersonalityProfileRequest"
+      )
+    )
+  )
+  @Path(value = "/{userId}/profile")
+  def modifyPersonalityProfile: Route
+
+  def routes: Route = getPersonality ~ getPersonalityProfile ~ modifyPersonalityProfile
 
 }
 
@@ -60,6 +112,100 @@ trait DefaultPersonalityApiComponent extends PersonalityApiComponent with MakeAu
         }
       }
 
-  }
+    override def getPersonalityProfile: Route = {
+      get {
+        path("personalities" / userId / "profile") { userId =>
+          makeOperation("GetPersonalityProfile") { _ =>
+            provideAsyncOrNotFound(userService.getPersonality(userId)) { user =>
+              complete(PersonalityProfileResponse.fromUser(user))
+            }
+          }
+        }
+      }
+    }
 
+    override def modifyPersonalityProfile: Route = {
+      put {
+        path("personalities" / userId / "profile") { personalityId =>
+          makeOperation("UpdatePersonalityProfile") { requestContext =>
+            decodeRequest {
+              entity(as[PersonalityProfileRequest]) { request =>
+                makeOAuth2 { currentUser =>
+                  authorize(currentUser.user.userId == personalityId) {
+                    provideAsyncOrNotFound(userService.getPersonality(personalityId)) { personality =>
+                      val modifiedProfile = personality.profile
+                        .orElse(Profile.parseProfile())
+                        .map(
+                          _.copy(
+                            avatarUrl = request.avatarUrl.map(_.value),
+                            description = request.description,
+                            optInNewsletter = request.optInNewsletter,
+                            website = request.website.map(_.value),
+                            politicalParty = Some(request.politicalParty)
+                          )
+                        )
+
+                      val modifiedPersonality = personality.copy(
+                        firstName = Some(request.firstName),
+                        lastName = Some(request.lastName),
+                        profile = modifiedProfile
+                      )
+
+                      provideAsync(userService.update(modifiedPersonality, requestContext)) { result =>
+                        complete(PersonalityProfileResponse.fromUser(result))
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+final case class PersonalityProfileResponse(
+  firstName: Option[String],
+  lastName: Option[String],
+  avatarUrl: Option[String],
+  description: Option[String],
+  optInNewsletter: Option[Boolean],
+  website: Option[String],
+  politicalParty: Option[String]
+)
+
+object PersonalityProfileResponse {
+  implicit val encoder: Encoder[PersonalityProfileResponse] = deriveEncoder[PersonalityProfileResponse]
+  implicit val decoder: Decoder[PersonalityProfileResponse] = deriveDecoder[PersonalityProfileResponse]
+
+  def fromUser(user: User): PersonalityProfileResponse = {
+    PersonalityProfileResponse(
+      firstName = user.firstName,
+      lastName = user.lastName,
+      avatarUrl = user.profile.flatMap(_.avatarUrl),
+      description = user.profile.flatMap(_.description),
+      website = user.profile.flatMap(_.website),
+      politicalParty = user.profile.flatMap(_.politicalParty),
+      optInNewsletter = user.profile.map(_.optInNewsletter)
+    )
+  }
+}
+
+final case class PersonalityProfileRequest(
+  firstName: String,
+  lastName: String,
+  @(ApiModelProperty @field)(dataType = "string", example = "https://example.com/logo.jpg")
+  avatarUrl: Option[String Refined Url],
+  description: Option[String],
+  optInNewsletter: Boolean,
+  @(ApiModelProperty @field)(dataType = "string", example = "https://make.org")
+  website: Option[String Refined Url],
+  politicalParty: String
+)
+
+object PersonalityProfileRequest {
+  implicit val encoder: Encoder[PersonalityProfileRequest] = deriveEncoder[PersonalityProfileRequest]
+  implicit val decoder: Decoder[PersonalityProfileRequest] = deriveDecoder[PersonalityProfileRequest]
 }
