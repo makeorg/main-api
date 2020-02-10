78794a34ec1d2347ee9a76d1f4920d67e702c546
Jean-Sébastien Bour <jean-sebastien.bour@make.org>
2020-02-10 15:00:27 +0100

    feat(transverse): add entrypoint to logs
    Create a log variable set to HTTP entrypoint or Kafka consumer class and
    message class.
    
    Closes MP-849

---
+++ api/src/main/java/org/make/api/technical/tracing/EntrypointPatternConverter.java
@@ -0,0 +1,47 @@
+/*
+ *  Make.org Core API
+ *  Copyright (C) 2020 Make.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.make.api.technical.tracing;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.pattern.ConverterKeys;
+import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
+import org.apache.logging.log4j.core.pattern.PatternConverter;
+
+@Plugin(name = EntrypointPatternConverter.name, category = PatternConverter.CATEGORY)
+@ConverterKeys({EntrypointPatternConverter.name})
+public class EntrypointPatternConverter extends LogEventPatternConverter {
+
+    static final String name = "entrypoint";
+
+    public static EntrypointPatternConverter newInstance(final String[] options) {
+        return new EntrypointPatternConverter(name, name);
+    }
+
+    protected EntrypointPatternConverter(String name, String style) {
+        super(name, style);
+    }
+
+    @Override
+    public void format(LogEvent event, StringBuilder toAppendTo) {
+        Tracing.entrypoint().foreach(toAppendTo::append);
+    }
+
+}
