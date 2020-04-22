20a85273e4ebcf03ba56041d75037d1bd9e023db
csalmon- <salmonl.colin@gmail.com>
2020-04-22 14:54:27 +0200

    fix(tag): add requiredForEnrichment to TagTypeResponse
    Closes MP-1064

--- api/src/main/scala/org/make/api/tagtype/TagTypeResponse.scala
+++ api/src/main/scala/org/make/api/tagtype/TagTypeResponse.scala
@@ -31,7 +31,8 @@ final case class TagTypeResponse(
   @(ApiModelProperty @field)(dataType = "string", example = "12345678-90ab-cdef-1234-567890abcdef") id: TagTypeId,
   label: String,
   @(ApiModelProperty @field)(dataType = "string", allowableValues = "DISPLAYED,HIDDEN") display: TagTypeDisplay,
-  weight: Int
+  weight: Int,
+  requiredForEnrichment: Boolean
 )
 
 object TagTypeResponse {
@@ -39,5 +40,11 @@ object TagTypeResponse {
   implicit val decoder: Decoder[TagTypeResponse] = deriveDecoder[TagTypeResponse]
 
   def apply(tagType: TagType): TagTypeResponse =
-    TagTypeResponse(id = tagType.tagTypeId, label = tagType.label, display = tagType.display, weight = tagType.weight)
+    TagTypeResponse(
+      id = tagType.tagTypeId,
+      label = tagType.label,
+      display = tagType.display,
+      weight = tagType.weight,
+      requiredForEnrichment = tagType.requiredForEnrichment
+    )
 }
