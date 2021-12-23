3406e1aff7286f350946eebe25caeb9f935fb8ad
csalmon- <cs@make.org>
2021-12-23 10:36:12 +0100

    feat(transverse): proposal scorer improvements
    - compile time count and smoothing
    - compute only once the counts with a normalised object VoteCounts
    
    Closes MP-2410

--- benchmarks/src/test/scala/org/make/benchmarks/Benchmarks.scala
+++ benchmarks/src/test/scala/org/make/benchmarks/Benchmarks.scala
@@ -18,27 +18,46 @@
  */
 package org.make.benchmarks
 
+import org.make.api.ProposalsUtils
+import org.make.api.proposal.ProposalScorer
+import org.make.api.proposal.ProposalScorer.VotesCounter.SequenceVotesCounter
+import org.make.api.sequence.SelectionAlgorithm.ExplorationSelectionAlgorithm
+import org.make.core.proposal.indexed.{IndexedProposal, IndexedScores}
+import org.make.core.sequence.{
+  ExplorationSequenceConfiguration,
+  ExplorationSequenceConfigurationId,
+  SequenceConfiguration
+}
 import org.openjdk.jmh.annotations._
-import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.infra.Blackhole
 
-import org.make.api.ProposalsUtils
-import org.make.api.sequence.SelectionAlgorithm.ExplorationSelectionAlgorithm
-import org.make.core.sequence.{ExplorationSequenceConfiguration, ExplorationSequenceConfigurationId}
-import org.make.core.proposal.indexed.IndexedProposal
+import java.util.concurrent.TimeUnit
 
 object States {
 
   private val (newProposals, testedProposals): (Seq[IndexedProposal], Seq[IndexedProposal]) =
     ProposalsUtils.getNewAndTestedProposals("sequence_simulation.csv")
 
+  private val scenariosProposal: Seq[IndexedProposal] =
+    ProposalsUtils.getProposalsFromCsv("scorer_simulation.csv").take(3).sortBy(_.votes.map(_.countVerified).sum)
+
   @State(Scope.Thread)
   class MyState {
     val configuration: ExplorationSequenceConfiguration =
       ExplorationSequenceConfiguration.default(ExplorationSequenceConfigurationId("bandit"))
     val nonSequenceRatio: Double = 0.5
-    val newProposals = States.newProposals
-    val testedProposals = States.testedProposals
+    val newProposals: Seq[IndexedProposal] = States.newProposals
+    val testedProposals: Seq[IndexedProposal] = States.testedProposals
+  }
+
+  @State(Scope.Thread)
+  class ScorerState {
+    val configuration: SequenceConfiguration = SequenceConfiguration.default
+    val nonSequenceRatio: Double = 0.5
+    val (zeroVotesProposal, standardVotesProposal, highVotesProposal) = States.scenariosProposal match {
+      case Seq(zero, standard, high) => (zero, standard, high)
+      case _                         => throw new IllegalStateException("Strictly 3 scenarios proposal needed for scorer benchmarks.")
+    }
   }
 }
 
@@ -61,5 +80,41 @@ class Benchmarks {
       None
     )
     blackhole.consume(chosenSequence)
+  }
+
+  @Benchmark
+  def scorerZeroVotesScore(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.zeroVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(IndexedScores(scorer))
+  }
+
+  @Benchmark
+  def scorerStandardVotesScore(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.standardVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(IndexedScores(scorer))
+  }
+
+  @Benchmark
+  def scorerHighVotesScore(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.highVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(IndexedScores(scorer))
+  }
+
+  @Benchmark
+  def scorerZeroVotesPool(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.zeroVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(scorer.pool(state.configuration, state.zeroVotesProposal.status))
+  }
+
+  @Benchmark
+  def scorerStandardVotesPool(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.standardVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(scorer.pool(state.configuration, state.standardVotesProposal.status))
+  }
+
+  @Benchmark
+  def scorerHighVotesPool(state: States.ScorerState, blackhole: Blackhole): Unit = {
+    val scorer = ProposalScorer(state.highVotesProposal.votes, SequenceVotesCounter, state.nonSequenceRatio)
+    blackhole.consume(scorer.pool(state.configuration, state.highVotesProposal.status))
   }
 }
